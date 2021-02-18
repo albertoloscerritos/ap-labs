6da235dc0106b93b515ecfcd67f8c19da44da28d
Alberto Rodriguez <a01630891@itesm.mx>
2021-02-17 22:22:18 -0600

    Tarea C-Basics
    Signed-off-by: Alberto Rodriguez <a01630891@itesm.mx>

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,17 +1,37 @@
+//Alberto Rodriguez
 #include <stdio.h>
+#include <stdlib.h>
 
-#define   LOWER  0       /* lower limit of table */
-#define   UPPER  300     /* upper limit */
-#define   STEP   20      /* step size */
+#define   LOWER  0       
+#define   UPPER  300     
+#define   STEP   20      
 
-/* print Fahrenheit-Celsius table */
-
-int main()
+int main(int argc, char **argv)
 {
-    int fahr;
+    int fahrenheit;
 
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    if(argc == 2){
+        fahrenheit = atoi(argv[1]);
+        printf("Fahrenheit: %3d, Celsius: %6.1f\n", fahrenheit, (5.0/9.0)*(fahrenheit-32));
+    }
+
+    if(argc < 2){
+        for (fahrenheit = LOWER; fahrenheit <= UPPER; fahrenheit = fahrenheit + STEP)
+	    printf("Fahrenheit: %3d, Celsius: %6.1f\n", fahrenheit, (5.0/9.0)*(fahrenheit-32));
+    }
+
+    else{
+        int f;       
+        
+         int inicio = atoi(argv[1]);
+         int final = atoi(argv[2]);
+         int inc = atoi(argv[3]);
+
+         for(f = inicio; f <= final; f = f + inc){
+             printf("Fahrenheit: %3d, Celsius: %6.1f\n", f, (5.0/9.0)*(f-32));
+         }
+
+    }
 
     return 0;
 }
