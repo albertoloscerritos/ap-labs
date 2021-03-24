df3d7d54103130c3502f826ba85cd4a616816075
Alberto Rodriguez <a01630891@itesm.mx>
2021-03-23 23:43:27 -0600

    str-lib
    Signed-off-by: Alberto Rodriguez <a01630891@itesm.mx>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+char *mystradd(char *,char *);
+int mystrfind(char *,char *);
+int mystrlen(char *);
+
+
+int main(int argc, char *argv[]) {
+    char *string = (char *)malloc(sizeof(char *)),
+         *string2 = (char *)malloc(sizeof(char *)),
+         *r = (char *)malloc(sizeof(char *));
+    string = argv[2];
+    string2 = argv[3];
+    if (mystrfind(argv[1],"-add")==0 && argc == 4)
+    {
+        printf("Initial Lenght\t: %d\n", mystrlen(string));
+        mystradd(string,string2);
+        printf("New String\t: %s\n", string);
+        printf("New lenght\t: %d\n", mystrlen(string));
+        return 0;
+    }
+    else if(mystrfind(argv[1],"-find")==0 && argc == 4)
+    {
+        printf("[%s] string was found at [%d] position\n", string2,mystrfind(string,string2));
+    }
+    else
+    {
+        printf("valor exp:\n");
+        printf("./main.o -add \"str\" \"str\"\n");
+        printf("- or -");
+        printf("./main.o -find \"str\" \"str\"\n");
+    }
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,43 @@
-
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int count = 0;
+    while (str[count] != '\0')
+    {
+        count++;
+    }
+    return count;
 }
 
-char *mystradd(char *origin, char *addition){
-    return 0;
+char *mystradd(char *dest, char *addition){
+    int count       = mystrlen(dest),
+        additionlen = mystrlen(addition);
+    for(int i = 0; i < additionlen; i++)
+    {
+        *(dest+count++) = *(addition+i);
+    }
+    *(dest+count) = '\0';
+    return dest;
 }
 
-int mystrfind(char *origin, char *substr){
-    return 0;
+int mystrfind(char *dest, char *substr){
+    int count       = 0,
+        limit       = mystrlen(substr),
+        destlen   = mystrlen(dest);
+    for(int i = 0; i < destlen; i++)
+    {
+        if(*(dest+i) == *(substr+count))
+        {
+            count++;
+        }
+        else 
+        {
+            i = i - count + 1;
+            count = 0;
+        }
+        if(count==limit){
+            return i - count + 1;
+        }
+    }
+    return -1;
 }
